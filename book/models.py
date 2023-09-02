4b0a89704c6a3ce1f892351fb9bc498484b24ff9
samandarsaburov <saburovsamandar@gmail.com>
2023-09-02 13:59:56 +0500

    commit book models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+class BookModel(models.Model):
+    title = models.CharField(max_length=25)
+    pages = models.IntegerField()
+    year = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    genre = models.CharField(max_length=50)
+    auther = models.CharField(max_length=25)
+    bio = models.TextField()
+    # user = models.ForeignKey(CustomUser,default='',on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title
+    class Meta:
+        db_table = 'book'
