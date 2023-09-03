86b87f5d0ee4b3ebed595d54ea440259ca14f92f
samandarsaburov <saburovsamandar@gmail.com>
2023-09-03 14:11:33 +0500

    auther book end

--- auther/admin.py
+++ auther/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from  .models import AutherModel
+from  .models import AutherModel, AutherBookModel
 # Register your models here.
 class AutherAdmin(admin.ModelAdmin):
     list_display= ['first_name','last_name','date_of_brth','place_of_birth','Date_of_death','Place_of_death','images','bio','genre']
     search_fields = ['first_name','']
+    
 admin.site.register(AutherModel,AutherAdmin)
+
+
+class AutherBooksAdmin(admin.ModelAdmin):
+    list_display = ['name','pages','year','price','genre','images','bio']
+    search_fields = ['name','genre']
+
+admin.site.register(AutherBookModel,AutherBooksAdmin) 
--- auther/models.py
+++ auther/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from datetime import date
 
+# Auther moduls
 class AutherModel(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
@@ -24,4 +24,24 @@ class AutherModel(models.Model):
         db_table = 'auther'
         
         
+# Auther Book
+class AutherBookModel(models.Model):
+    name = models.CharField(max_length=25)
+    pages = models.IntegerField()
+    year = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(upload_to='images/',default='')
+    bio = models.TextField()
+    GENRE_CHOICES = (
+    ('Adventure', 'Adventure'),
+    ('Science Fiction', 'Science Fiction'),
+    ('Romance', 'Romance'),
+    # Boshqa variantlar...
+    )
+    genre =models.CharField(max_length=50, choices=GENRE_CHOICES)
         
+    def __str__(self) -> str:
+        return self.name
+        
+    class Meta:
+        db_table = 'autherbook'
